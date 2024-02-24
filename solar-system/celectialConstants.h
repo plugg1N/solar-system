8320712d44f4b4d175af628dbbc0be41dcd9118c
plugg1N <bbulkaskoricey87@gmail.com>
2024-02-24 22:08:52 +0300

    last commit

--- solar-system/celectialConstants.h
+++ solar-system/celectialConstants.h
@@ -3,7 +3,23 @@
 
 const double AU = 149.6e6 * 1000;
 const double G = 6.67428e-11;
-const double SCALE = 170 / AU;
 const double TIMESTEP = 3600*24;
+const double SCALE = 200 / AU;
+
+
+// Distance of planets in AU
+const double dEAR = 1;
+const double dMAR = 1.524;
+const double dMER = 0.387;
+const double dVEN = 0.723;
+
+// Mass of planets
+const double mSUN = 1.98892 * 10e30;
+const double mEAR = 5.9742 * 10e24;
+const double mMAR = 6.39 * 10e23;
+const double mMER = 3.30 * 10e23;
+const double mVEN = 4.8685 * 10e24;
+
+
 
 #endif
--- solar-system/main.cpp
+++ solar-system/main.cpp
@@ -6,10 +6,10 @@
 
 
 
+// Create a Planet class
 class Planet {
 public:
     std::string name;
-    static const double sun_collision_radius;
     double x, y, radius, mass, distance_to_sun;
     bool sun;
     double x_vel, y_vel;
@@ -17,10 +17,14 @@ public:
 
     std::vector<sf::Vector2f> orbit;
 
+    // Initialize a class
     Planet(const std::string& planetName, double x, double y, double radius, sf::Color color, double mass)
         : name(planetName), x(x), y(y), radius(radius), color(color), mass(mass), sun(false), distance_to_sun(0), x_vel(0), y_vel(0) {}
 
+
+    // Draw method
     void draw(sf::RenderWindow& win) {
+        // Center of the screen
         double screen_x = x * SCALE + win.getSize().x / 2;
         double screen_y = y * SCALE + win.getSize().y / 2;
 
@@ -34,7 +38,7 @@ public:
 
         win.draw(line);
 
-
+        // Draw the orbit
         if (orbit.size() > 2) {
             std::vector<sf::Vector2f> updated_points;
             for (const auto& point : orbit) {
@@ -52,15 +56,19 @@ public:
             win.draw(lines);
         }
 
+        // Draw a planet
         sf::CircleShape circle(radius);
         circle.setFillColor(color);
         circle.setPosition(screen_x - radius, screen_y - radius);
         win.draw(circle);
 
+
+        // Handle texts
         if (!sun) {
             sf::Font font;
             font.loadFromFile("arial.ttf");
 
+            // Name of each planet
             sf::Text name_text;
             name_text.setFont(font);
             name_text.setString(name);
@@ -69,9 +77,11 @@ public:
             name_text.setPosition(screen_x - radius / 2 - 30.f, screen_y - radius / 2 - 30.f);
             win.draw(name_text);
 
+
+            // Distance from Sun
             sf::Text distance_text;
             distance_text.setFont(font);
-            distance_text.setString(std::to_string(static_cast<int>(distance_to_sun / 1000000)) + "M km");
+            distance_text.setString(std::to_string(static_cast<int>(distance_to_sun / 1000000000 /*Since everything is in meteres*/)) + "M km");
             distance_text.setCharacterSize(16);
             distance_text.setFillColor(sf::Color::Green);
             distance_text.setPosition(screen_x - radius / 2 - 30.f, screen_y - radius / 2 + 20.f);
@@ -79,6 +89,8 @@ public:
         }
     }
 
+
+    // Calculate Newton's Law of Universal Gravitation
     sf::Vector2f attraction(const Planet& other) {
         double distance_x = other.x - x;
         double distance_y = other.y - y;
@@ -89,13 +101,16 @@ public:
         }
 
 
-        double force = G * mass * other.mass / (distance * distance) * 0.09f;
+        // Main formula
+        double force = G * mass * other.mass / (distance * distance) * 0.096f /*excentric const*/;
         double theta = std::atan2(distance_y, distance_x);
         double force_x = std::cos(theta) * force;
         double force_y = std::sin(theta) * force;
         return sf::Vector2f(force_x, force_y);
     }
 
+
+    // Update position of each planet
     void update_position(const std::vector<Planet>& planets) {
         double total_fx = 0, total_fy = 0;
         for (const auto& planet : planets) {
@@ -117,6 +132,8 @@ public:
     }
 };
 
+
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(1700, 900), "Planet Simulation");
     window.setFramerateLimit(89);
@@ -124,26 +141,33 @@ int main() {
     std::vector<Planet> planets;
 
 
-
-
-    Planet sun("Sun", 0, 50, 40, sf::Color::Yellow, 1.98892 * 10e30);
+    // Create an object of each planet chosen
+    Planet sun("Sun", 0, 50, 40, sf::Color::Yellow, mSUN);
     sun.sun = true;
 
-    Planet earth("Earth", 1 * AU, 600, 12, sf::Color(100, 149, 237), 5.9742 * 10e24);
+    Planet earth("Earth", dEAR * AU, 0, 12, sf::Color(100, 149, 237), mEAR);
     earth.y_vel = 29.783 * 1000;
 
-    Planet mars("Mars", 1.524 * AU, 700, 8, sf::Color(188, 39, 50), 6.39 * 10e23);
+    Planet mars("Mars", dMAR * AU, 0, 8, sf::Color(188, 39, 50), mMAR);
     mars.y_vel = 24.077 * 1000;
 
-    Planet mercury("Mercury", 0.387 * AU, 800, 4, sf::Color(80, 78, 81), 3.30 * 10e23);
+    Planet mercury("Mercury", dMER * AU, 0, 4, sf::Color(80, 78, 81), mMER);
     mercury.y_vel = 47.4 * 1000;
 
-    Planet venus("Venus", 0.723 * AU, 900, 10, sf::Color::White, 4.8685 * 10e24);
+    Planet venus("Venus", dVEN * AU, 0, 10, sf::Color::White, mVEN);
     venus.y_vel = 35.02 * 1000;
 
+    /*Planet jupiter("Jupiter", 5.203 * AU, 0, 131, sf::Color::White, 1.8986 * 10e27);
+    jupiter.y_vel = 13.07 * 1000;*/
 
 
-    planets = { sun, earth, mars, mercury, venus };
+
+    planets = { sun, earth, mars, mercury, venus, /*jupiter*/};
+
+    // Set bg texture
+    sf::Texture t;
+    t.loadFromFile("bg.jpg");
+    sf::Sprite s(t);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -153,14 +177,21 @@ int main() {
             }
         }
 
+
         window.clear();
 
+        // Draw bg
+        window.draw(s);
+
+
+        // Draw each planet
         for (auto& planet : planets) {
             planet.update_position(planets);
             planet.draw(window);
         }
 
 
+        // TimeStep at the left-most corner of screen
         sf::Font font;
         font.loadFromFile("arial.ttf");
 
@@ -172,7 +203,9 @@ int main() {
         info_time_step.setPosition(14.f, 14.f);
 
 
+        
         window.draw(info_time_step);
+
         window.display();
     }
 
